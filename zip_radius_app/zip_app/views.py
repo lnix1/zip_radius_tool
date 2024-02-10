9af18fb57ac839fb3964a6c0c8e59448ebe9c6bc
Louis Nix II <louis.nix2@gmail.com>
2024-02-10 14:52:46 -0800

    got the index page redering and submission form added

--- zip_radius_app/zip_app/views.py
+++ zip_radius_app/zip_app/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from uszipcode import SearchEngine
+import pandas as pd
+from django.views.decorators.csrf import csrf_exempt
 
+def pull_radius_zips(f):
+    radius_results = pd.DataFrame(columns = ['radius_zips', 'original_zips'])
+    sr = SearchEngine()
+    for z in f['zip_codes']:
+        zip_info = sr.by_zipcode(str(z))
+        lat = zip_info.lat
+        lng = zip_info.lng
+        result = sr.by_coordinates(lat, lng, radius = 10, returns = 0)
+        radius = []
+        for i in result:
+            radius.append(i.zipcode)
+        radius = pd.DataFrame(radius)
+        radius['original_zip'] = z
+        radius.columns = ['radius_zips', 'original_zips']
+        radius_results = pd.concat([radius_results, radius], axis = 0)
+    return radius_results
+
+@csrf_exempt
 # Create your views here.
 def index(request):
     context = {}
+    if request.method == 'POST':
+        f = request.FILES['csvFile']
+        print(type(f))
+        radius_results = pull_radius_zips(f)
+        print('checking this works to this point')
     return render(request, "zip_app/index.html", context)
--- zip_radius_app/zip_radius_app/settings.py
+++ zip_radius_app/zip_radius_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'zip_app.apps.ZipAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
